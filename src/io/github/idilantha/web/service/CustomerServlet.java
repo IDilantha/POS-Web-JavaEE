1f060c567303f596cbe30cca7ae3047a5aee6d42
IDilantha <isurudilantha@gmail.com>
2020-01-19 16:47:11 +0530

    Create Customer servlet

---
+++ src/io/github/idilantha/web/service/CustomerServlet.java
@@ -0,0 +1,9 @@
+package io.github.idilantha.web.service;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/api/v1/customers")
+public class CustomerServlet extends HttpServlet {
+
+}
