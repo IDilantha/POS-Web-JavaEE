0b99bf642338d18957dd2b6a604d0bbca0721f47
IDilantha <isurudilantha@gmail.com>
2020-01-19 17:06:14 +0530

    Create DBConnection file

---
+++ src/io/github/idilantha/web/db/DBConnection.java
@@ -0,0 +1,29 @@
+package io.github.idilantha.web.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC7","root","T42dKML&");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DBConnection getDbConnection(){
+        return (dbConnection == null) ? (dbConnection = new DBConnection()) : dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
