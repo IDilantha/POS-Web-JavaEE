18cd5cdf6dd6ab7c966f672f73a604aee4e8e80a
IDilantha <isurudilantha@gmail.com>
2020-01-20 11:02:36 +0530

    Create CORSFilter

---
+++ src/io/github/idilantha/web/filter/CORSFilter.java
@@ -0,0 +1,15 @@
+package io.github.idilantha.web.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+}
